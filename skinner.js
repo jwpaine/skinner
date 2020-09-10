1a5fadde2c4ea6a01333a36ab9091451f37f0fda
jwpaine <jwpaine@users.noreply.github.com>
2020-09-10 16:09:45 -0400

    Push

--- skinner.js
+++ skinner.js
@@ -1,27 +1,49 @@
 const readline = require("readline");
 const fs = require('fs')
+const Rsync = require('rsync');
+
+
 
 let defaultConfig = {
-	"host" : "ec2-3-236-151-5.compute-1.amazonaws.com",
+	"host" : "ubuntu@ec2-3-236-151-5.compute-1.amazonaws.com",
 	"base_dir" : "/avetti/httpd/htdocs/content/preview/store/",
-	"shop" : "",
-	"working_dir" : "/assets/themes/blaze_en/css/src"
+	"shop" : "20190723594",
+	"working_dir" : "/assets/themes/blaze_en/css"
 }
 
 let siteConfig = {
 	"host" : "ec2-3-236-151-5.compute-1.amazonaws.com",
 	"base_dir" : "/avetti/httpd/htdocs/content/preview/store/",
 	"shop" : "20190723594",
-	"working_dir" : "/assets/themes/blaze_en/css/src"
+	"working_dir" : "/assets/themes/blaze_en/css"
 }
 
-function Shop(config) {
-	this.config = config
+function Shop(working_dir, config) {
+	 this.working_dir = working_dir
+	 this.config = config
 }
 
-Shop.prototype.init = function(config) {
+Shop.prototype.init = function(callback) {
 	console.log(`Initializing shop for local development`)
+	generateConfig.init(defaultConfig, (config) => {
+		// preserve supplied config and working_dir values
+		this.config = config
+		this.working_dir = process.cwd() + '/' + config.shop
 
+		console.log(`--> ${JSON.stringify(this.config)}`)
+		console.log(`creating directory ${process.cwd() + '/' + this.config.shop}`)
+
+		fs.mkdir(this.working_dir, (error) => {
+			if (error) { 
+				throw error
+			}  	
+			console.log(`Local directory ${this.config.shop} created`)
+			console.log(`Writing config: ${this.working_dir + '/.config'}`)
+			fs.writeFile(this.working_dir + '/.config', JSON.stringify(this.config), (error) => {
+				callback(error, this)
+			}) 
+		})
+	})
 }
 
 Shop.prototype.push = function() {
@@ -29,7 +51,27 @@ Shop.prototype.push = function() {
 }
 
 Shop.prototype.pull = function() {
-	console.log(`Syncing remote -> local`)
+	
+
+	let source = `${this.config.host}:${this.config.base_dir + this.config.shop + this.config.working_dir}`
+
+	console.log(`pulling ${source} -> ${this.working_dir}`)
+
+
+	var rsync = new Rsync()
+	.shell('ssh')
+	.flags('az')
+	.source(source)
+	.destination(this.working_dir);
+
+	// Execute the command
+	rsync.execute(function(error, code, cmd) {
+	// we're done
+		if (error) {
+			console.log(error)
+		}
+		console.log('done!')
+	});
 }
 
 Shop.prototype.watch = function(local_dir, action) {
@@ -46,8 +88,6 @@ generateConfig.init = function(defaultConfig, callback) {
 	});
 	
 	let config = {}
-
- 
 	let keys = Object.keys(defaultConfig)
 	let i = 0;
 	
@@ -77,26 +117,18 @@ generateConfig.init = function(defaultConfig, callback) {
 }
 
 
-// let shop = new Shop(siteConfig)
-
 if (process.argv[2] == 'init') {
-	generateConfig.init(defaultConfig, (config) => {
-		console.log(`--> ${JSON.stringify(config)}`)
-		console.log(`creating directory ${process.cwd() + '/' + config.shop}`)
-		let working_dir = process.cwd() + '/' + config.shop
-		fs.mkdir(working_dir, (error) => {
-			if (error) { 
-				throw error
-			}  
-			
-			console.log(`Local directory ${config.shop} created`)
-			console.log(`Writing config: ${working_dir + '/.config'}`)
-			fs.writeFile(working_dir + '/.config', JSON.stringify(config), (error) => {
-				if (error) {
-					throw(error)
-				}
-				console.log('Config written to disk')
-			}) 
-		})
+	new Shop(null, null).init((error, shop) => {
+		if (error) throw error
+
+		shop.pull()
+		
 	})
-}	
+}
+
+// test pull
+let working_dir = process.cwd() + '/' + defaultConfig.shop
+test = new Shop(working_dir, defaultConfig)
+test.pull()
+
+
